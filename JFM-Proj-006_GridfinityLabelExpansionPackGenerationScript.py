0fbdfae4fb561c74d20d8eeda22601f398ab4e13
jakefrommars64 <118164189+jakefrommars64@users.noreply.github.com>
2024-06-12 10:25:33 -0400

    Getting files and resources ready

--- JFM-Proj-006_GridfinityLabelExpansionPackGenerationScript.py
+++ JFM-Proj-006_GridfinityLabelExpansionPackGenerationScript.py
@@ -1,15 +1,16 @@
-#Author-
-#Description-
+# Author-Jacob Davis
+# Description-
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
+
 
 def run(context):
     ui = None
     try:
         app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Hello script')
+        ui = app.userInterface
+        ui.messageBox("Hello script")
 
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
